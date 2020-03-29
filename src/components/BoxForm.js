6a9cb403df7ba4611c4bd751fb043b175c4ec2f2
gpm96c <griffinpotts96@gmail.com>
2020-03-29 11:28:58 -0700

    paralax added

--- src/components/BoxForm.js
+++ src/components/BoxForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -7,10 +7,23 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TablePagination from '@material-ui/core/TablePagination';
+
 import { Controls } from './Controls';
 
 export const BoxForm = (props) => {
   const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
   return (
     <BoxStyle>
       <span className="titlebar">
@@ -61,7 +74,7 @@ export const BoxForm = (props) => {
           </TableBody>
         </Table>
       </TableContainer>
-    {props.controls && <Controls options={props.controls}/>}
+          {props.controls && <Controls options={props.controls}/>}
     </BoxStyle>
   )
 }
@@ -74,12 +87,11 @@ border-radius: 3px;
 box-shadow: 6px 6px 13px -2px rgba(0,0,0,0.25);
 display:flex;
 flex-direction: column;
-/* background-image: url('https://i.imgur.com/Q8ECZEy.png');
+background-image: url('https://i.imgur.com/Q8ECZEy.png');
 background-attachment: fixed;
   background-position: center;
   background-repeat: no-repeat;
-  background-size: cover; */
-background:#ffffff;
+  background-size: cover;
 *{
   overflow-y:visible;
 }
